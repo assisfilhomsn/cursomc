78b71e86a0f771a421b3415c6cf822d0e5a35c7d
Assis Lima <assisfilho.msn@gmail.com>
2019-05-10 21:21:28 -0300

    Produto e assiciacao muitos para muitos

---
+++ src/main/java/com/assislima/cursomc/repositories/ProdutoRepository.java
@@ -0,0 +1,11 @@
+package com.assislima.cursomc.repositories;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import com.assislima.cursomc.domain.Produto;
+
+@Repository
+public interface ProdutoRepository extends JpaRepository<Produto, Integer>  {
+	
+}
